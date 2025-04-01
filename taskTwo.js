f49256e1ceecde13820dc591afbd3e9caedfc7fe
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-04-01 09:27:37 +0100

    task two done

---
+++ taskTwo.js
@@ -0,0 +1,26 @@
+// // Task-2
+// Write a program to calculate the average marks of Mathematics, Biology, Chemistry, Physics, and Bangla of a student.
+
+// Input:
+// The first line of the input is the marks of the five subjects mentioned above, respectively.
+
+// Output:
+// Print the result in 2 decimal places.
+
+// Sample Input:
+// 75.25, 65, 80, 35.45, 99.50
+
+// Sample Output:
+// 71.04
+
+var mathNumber = 76.5;
+var physicsNumber = 78.5;
+var chemistryNumber = 76.5;
+var banglaNumber = 77.5;
+
+var avgNumber = (mathNumber+physicsNumber+chemistryNumber+banglaNumber)/4;
+
+var result;
+
+result = (mathNumber+physicsNumber+chemistryNumber+banglaNumber)/4;
+console.log(result);
